a25525b6acb2adc36d6dd376ac02ac5239e25de1
Marcin Zdun <mzdun@midnightbits.com>
2022-06-20 20:02:57 +0200

    Add bytes::subview

--- libs/git2-c++/include/git2-c++/bytes.hh
+++ libs/git2-c++/include/git2-c++/bytes.hh
@@ -3,7 +3,9 @@
 
 #pragma once
 #include <git2/buffer.h>
+#include <concepts>
 #include <cstddef>
+#include <string_view>
 
 namespace git {
 	struct bytes {
@@ -17,12 +19,19 @@ namespace git {
 		constexpr bytes(std::byte const* data, size_type length) noexcept
 		    : data_{data}, length_{length} {}
 
-		bytes(char const* data, size_type length) noexcept
+		template <typename Byte>
+		requires(!std::same_as<std::byte, Byte> && (sizeof(Byte) == 1))
+		    bytes(Byte const* data, size_type length) noexcept
 		    : data_{reinterpret_cast<std::byte const*>(data)}
 		    , length_{length} {}
 
 		explicit bytes(git_buf const& buf) noexcept
 		    : bytes(buf.ptr, static_cast<size_type>(buf.size)) {}
+
+		template <typename Byte>
+		requires(sizeof(Byte) == 1) explicit bytes(
+		    std::basic_string_view<Byte> const& view) noexcept
+		    : bytes(view.data(), view.size()) {}
 
 		constexpr std::byte const* data() const noexcept { return data_; }
 		constexpr std::byte const* begin() const noexcept { return data_; }
@@ -30,6 +39,17 @@ namespace git {
 			return data_ + length_;
 		}
 		constexpr size_type size() const noexcept { return length_; }
+
+		bytes subview(size_t offset = 0) const noexcept {
+			if (offset > length_) offset = length_;
+			return {data_ + offset, length_ - offset};
+		}
+
+		bytes subview(size_t offset, size_t length) const noexcept {
+			if (offset > length_) offset = length_;
+			if ((length_ - offset) < length) length = length_ - offset;
+			return {data_ + offset, length};
+		}
 
 	private:
 		std::byte const* data_{};
---
+++ libs/git2-c++/tests/bytes-test.cc
@@ -0,0 +1,35 @@
+// Copyright (c) 2022 midnightBITS
+// This code is licensed under MIT license (see LICENSE for details)
+
+#include <gtest/gtest.h>
+#include <git2-c++/bytes.hh>
+#include "setup.hh"
+
+namespace git::testing {
+	using namespace ::std::literals;
+
+	static constexpr auto digits = "0123456789"sv;
+
+	std::string_view to_view(bytes b) {
+		return {reinterpret_cast<char const*>(b.data()), b.size()};
+	}
+
+	TEST(bytes, offset_only) {
+		ASSERT_EQ(digits, to_view(bytes{digits}.subview()));
+		ASSERT_EQ(digits, to_view(bytes{digits}.subview(0)));
+		ASSERT_EQ(digits.substr(3), to_view(bytes{digits}.subview(3)));
+	}
+
+	TEST(bytes, length_only) {
+		ASSERT_EQ(digits.substr(0, 3), to_view(bytes{digits}.subview(0, 3)));
+		ASSERT_EQ(digits.substr(0, 7), to_view(bytes{digits}.subview(0, 7)));
+		ASSERT_EQ(digits, to_view(bytes{digits}.subview(0, 10)));
+		ASSERT_EQ(digits, to_view(bytes{digits}.subview(0, 15)));
+	}
+
+	TEST(bytes, subview) {
+		ASSERT_EQ(digits.substr(2, 3), to_view(bytes{digits}.subview(2, 3)));
+		ASSERT_EQ(digits.substr(2, 7), to_view(bytes{digits}.subview(2, 7)));
+		ASSERT_EQ(digits.substr(5, 5), to_view(bytes{digits}.subview(5, 7)));
+	}
+}  // namespace git::testing
