cffae80ddf87d863cc1abcc0e2fe4e1514fdcc7a
Marcin Zdun <mzdun@midnightbits.com>
2022-06-28 18:45:02 +0200

    Add report IO

---
+++ libs/cov/include/cov/io/report.hh
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 midnightBITS
+// This code is licensed under MIT license (see LICENSE for details)
+
+#pragma once
+
+#include <cov/io/db_object.hh>
+#include <cov/object.hh>
+
+namespace cov::io::handlers {
+	struct report : db_handler_for<cov::report> {
+		ref<counted> load(uint32_t magic,
+		                  uint32_t version,
+		                  read_stream& in,
+		                  std::error_code& ec) const override;
+		bool store(ref<counted> const& obj, write_stream& out) const override;
+	};
+}  // namespace cov::io::handlers
