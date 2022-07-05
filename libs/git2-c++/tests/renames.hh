3035f5f6197779cdf9703e83b5ba0cd04dd03609
Marcin Zdun <mzdun@midnightbits.com>
2022-07-05 18:53:36 +0200

    Make diff tests independent from this repo

---
+++ libs/git2-c++/tests/renames.hh
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Marcin Zdun
+// This code is licensed under MIT license (see LICENSE for details)
+
+#pragma once
+#include <git2/repository.hh>
+
+namespace git::testing::renames {
+	repository open_repo();
+}  // namespace git::testing::renames
