cd7e1e5bba27adfb0feba1b6a2464f1e9966f75b
Marcin Zdun <mzdun@midnightbits.com>
2022-07-23 08:27:11 +0200

    Add strings for cov module

--- tools/add_lngs.py
+++ tools/add_lngs.py
@@ -6,6 +6,7 @@ import sys
 name = sys.argv[1]
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 app = os.path.join(root, "libs", "app")
+cov_rt = os.path.join(root, "libs", "cov-rt", "include", "cov", "app")
 
 with open(os.path.join(root, "CMakeLists.txt"), "r") as f:
     for attr, value in [
@@ -44,6 +45,29 @@ touch(
 }}""",
 )
 
-os.makedirs(os.path.join(app, "data", "translations", name), exist_ok=True)
 touch(os.path.join(app, "include", "cov", "app", "strings", f"{name}.hh"))
 touch(os.path.join(app, "src", "strings", f"{name}.cc"))
+short = name
+if short[:4] == "cov_":
+    short = short[4:]
+Short = short.capitalize()
+touch(
+    os.path.join(cov_rt, f"{name}_tr.hh"),
+    f"""// Copyright (c) 2022 Marcin Zdun
+// This code is licensed under MIT license (see LICENSE for details)
+
+#pragma once
+
+#include <cov/app/strings/{name}.hh>
+#include <cov/app/tr.hh>
+
+namespace cov::app {{
+	using {short}lng = str::{name}::lng;
+	using {Short}Strings = str::{name}::Strings;
+
+	template <>
+	struct lngs_traits<{short}lng>
+	    : base_lngs_traits<{short}lng, "{name}", {Short}Strings> {{}};
+}}  // namespace cov::app
+""",
+)
