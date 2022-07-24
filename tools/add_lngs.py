89e06205f1a26df04f0a9120ea3205b7ce07ad43
Marcin Zdun <mzdun@midnightbits.com>
2022-07-24 09:14:05 +0200

    Add python linting

--- tools/add_lngs.py
+++ tools/add_lngs.py
@@ -8,13 +8,20 @@ root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 app = os.path.join(root, "libs", "app")
 
 with open(os.path.join(root, "CMakeLists.txt"), "r") as f:
-    for attr, value in [line.strip().split(' ', 1) for line in f.read().split(
-        'project (cov', 1)[1].split(')', 1)[0].strip().split('\n')]:
-        if attr == 'VERSION':
+    for attr, value in [
+        line.strip().split(" ", 1)
+        for line in f.read()
+        .split("project (cov", 1)[1]
+        .split(")", 1)[0]
+        .strip()
+        .split("\n")
+    ]:
+        if attr == "VERSION":
             VERSION = value
             break
 
-def touch(filename, text = None):
+
+def touch(filename, text=None):
     print(filename)
     if text is None:
         try:
@@ -22,17 +29,20 @@ def touch(filename, text = None):
         except FileNotFoundError:
             open(filename, "w")
         return
-    with open(filename, "w", encoding='UTF-8') as f:
+    with open(filename, "w", encoding="UTF-8") as f:
         print(text, file=f)
 
 
-touch(os.path.join(app, "data", "strings", f"{name}.lngs"), f'''[
+touch(
+    os.path.join(root, "data", "strings", f"{name}.lngs"),
+    f"""[
     project("cov"),
-	namespace("cov::app::str::{name}"),
+    namespace("cov::app::str::{name}"),
     version("{VERSION}"),
     serial(1)
 ] strings {{
-}}''')
+}}""",
+)
 
 os.makedirs(os.path.join(app, "data", "translations", name), exist_ok=True)
 touch(os.path.join(app, "include", "cov", "app", "strings", f"{name}.hh"))
