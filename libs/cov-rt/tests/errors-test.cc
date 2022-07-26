787dc0cb0dbe0ff16e4a3403d3b240006f8790e5
Marcin Zdun <mzdun@midnightbits.com>
2022-07-26 20:34:12 +0200

    Move args composition from cov RT to app lib

--- libs/cov-rt/tests/errors-test.cc
+++ libs/cov-rt/tests/errors-test.cc
@@ -26,6 +26,56 @@ namespace cov::app::testing {
 	};
 
 	class error : public ::testing::TestWithParam<error_test> {};
+
+	TEST(error, args) {
+		git::init globals{};
+		std::vector<std::string> en{"en"};
+		str::args_translator<> tr{
+		    {platform::sys_root() / directory_info::share / "locale"sv, en}};
+
+		using lng = str::args::lng;
+		using parser = base_parser<>;
+		using args_description = parser::args_description;
+		using visitor = parser::string::str_visitor;
+
+		static constexpr auto descr_00 =
+		    args_description{"-a, --arg"sv, lng::HELP_DESCRIPTION,
+		                     lng::FILE_META, lng::DEF_META};
+		static constexpr auto descr_01 =
+		    args_description{"-a, --arg"sv, lng::HELP_DESCRIPTION,
+		                     opt{lng::FILE_META}, lng::DEF_META};
+		static constexpr auto descr_10 =
+		    args_description{"-a, --arg"sv, lng::HELP_DESCRIPTION,
+		                     lng::FILE_META, opt{lng::DEF_META}};
+		static constexpr auto descr_11 =
+		    args_description{"-a, --arg"sv, lng::HELP_DESCRIPTION,
+		                     opt{lng::FILE_META}, opt{lng::DEF_META}};
+		static constexpr auto descr_0 = args_description{
+		    "-a, --arg"sv, lng::HELP_DESCRIPTION, lng::FILE_META};
+		static constexpr auto descr_1 = args_description{
+		    "-a, --arg"sv, lng::HELP_DESCRIPTION, opt{lng::FILE_META}};
+
+		visitor V{tr};
+#define ASSERT_EQUAL(FIRST, SECOND, DESCR)          \
+	do {                                            \
+		auto const [first, second] = DESCR.with(V); \
+		ASSERT_EQ(FIRST, first);                    \
+		ASSERT_EQ(SECOND, second);                  \
+	} while (0)
+		ASSERT_EQUAL("-a, --arg <file> <arg>"sv,
+		             "shows this help message and exits"sv, descr_00);
+		ASSERT_EQUAL("-a, --arg [<file>] <arg>"sv,
+		             "shows this help message and exits"sv, descr_01);
+		ASSERT_EQUAL("-a, --arg <file> [<arg>]"sv,
+		             "shows this help message and exits"sv, descr_10);
+		ASSERT_EQUAL("-a, --arg [<file>] [<arg>]"sv,
+		             "shows this help message and exits"sv, descr_11);
+		ASSERT_EQUAL("-a, --arg <file>"sv,
+		             "shows this help message and exits"sv, descr_0);
+		ASSERT_EQUAL("-a, --arg [<file>]"sv,
+		             "shows this help message and exits"sv, descr_1);
+	};
+
 	TEST_P(error, message_en) {
 		auto const& [git, ec, expected] = GetParam();
 		git::init globals{};
@@ -79,26 +129,31 @@ namespace cov::app::testing {
 	        .git = {GIT_ERROR_REGEX, "error message goes here"},
 	        .expected =
 	            {.en = "cov config: regex error: error message goes here"sv,
-	             .pl = "cov config: b\xC5\x82\xC4\x85" "d regex: error message goes here"sv},
+	             .pl = "cov config: b\xC5\x82\xC4\x85"
+	                   "d regex: error message goes here"sv},
 	    },
 	    {
 	        .git = {GIT_ERROR_CONFIG, "error message goes here"},
 	        .expected = {.en = "cov config: error: error message goes here"sv,
-	                     .pl = "cov config: b\xC5\x82\xC4\x85" "d: error message goes here"sv},
+	                     .pl = "cov config: b\xC5\x82\xC4\x85"
+	                           "d: error message goes here"sv},
 	    },
 	    {
 	        .git = {GIT_ERROR_INDEX, "neither regex nor config"},
 	        .expected = {.en = "cov config: error: neither regex nor config"sv,
-	                     .pl = "cov config: b\xC5\x82\xC4\x85" "d: neither regex nor config"sv},
+	                     .pl = "cov config: b\xC5\x82\xC4\x85"
+	                           "d: neither regex nor config"sv},
 	    },
 	    {
 	        .ec = std::make_error_code(std::errc::not_a_directory),
 #ifdef _MSC_VER
 	        .expected = {.en = "cov config: generic error: not a directory"sv,
-	                     .pl = "cov config: b\xC5\x82\xC4\x85" "d generic: not a directory"sv},
+	                     .pl = "cov config: b\xC5\x82\xC4\x85"
+	                           "d generic: not a directory"sv},
 #else
 	        .expected = {.en = "cov config: generic error: Not a directory"sv,
-	                     .pl = "cov config: b\xC5\x82\xC4\x85" "d generic: Not a directory"sv},
+	                     .pl = "cov config: b\xC5\x82\xC4\x85"
+	                           "d generic: Not a directory"sv},
 #endif
 	    },
 	};
