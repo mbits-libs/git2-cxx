5b8060e3b023c431f4d63c6b0866c645e1d06d3d
Marcin Zdun <mzdun@midnightbits.com>
2022-08-01 17:46:42 +0200

    feat(rt): setup code for reports

---
+++ libs/cov-rt/include/cov/app/report.hh
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 Marcin Zdun
+// This code is licensed under MIT license (see LICENSE for details)
+
+#pragma once
+
+namespace cov::app::report {
+}  // namespace cov::app::report
