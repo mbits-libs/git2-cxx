f4df01cdac97cac9c2fd672d92c58272d79e6baf
Marcin Zdun <mzdun@midnightbits.com>
2022-08-03 15:18:59 +0200

    feat(rt): load additional info from Git repo

--- libs/cov-rt/include/cov/app/report.hh
+++ libs/cov-rt/include/cov/app/report.hh
@@ -3,15 +3,22 @@
 
 #pragma once
 
+#include <cov/io/types.hh>
+#include <filesystem>
+#include <git2/repository.hh>
+#include <git2/tree.hh>
 #include <map>
 #include <string>
 #include <string_view>
+#include <type_traits>
 #include <vector>
 
 namespace cov::app::report {
+	enum class digest { unknown, md5, sha1 };
+
 	struct file_info {
 		std::string name{};
-		std::string algorithm{};
+		report::digest algorithm{digest::unknown};
 		std::string digest{};
 		std::map<unsigned, unsigned> line_coverage{};
 		auto operator<=>(file_info const&) const noexcept = default;
@@ -29,5 +36,50 @@ namespace cov::app::report {
 
 		auto operator<=>(report_info const&) const noexcept = default;
 		bool load_from_text(std::string_view u8_encoded);
+	};
+
+	struct git_signature {
+		std::string name;
+		std::string mail;
+	};
+
+	enum class text {
+		missing = 0x0000'0000,
+		in_repo = 0x0000'0001,
+		in_fs = 0x0000'0002,
+		different_newline = 0x0000'0004,
+		mismatched = 0x0000'0008,
+	};
+
+	constexpr inline text operator|(text lhs, text rhs) {
+		using underlying = std::underlying_type_t<text>;
+		return static_cast<text>(static_cast<underlying>(lhs) |
+		                         static_cast<underlying>(rhs));
+	}
+
+	constexpr inline text operator&(text lhs, text rhs) {
+		using underlying = std::underlying_type_t<text>;
+		return static_cast<text>(static_cast<underlying>(lhs) &
+		                         static_cast<underlying>(rhs));
+	}
+
+	struct blob_info {
+		text flags{text::missing};
+		git_oid existing{};
+	};
+
+	struct git_commit {
+		git_signature author{};
+		git_signature committer{};
+		sys_seconds committed{};
+		std::string message{};
+		git::tree tree{};
+
+		static git_commit load(git::repository_handle repo,
+		                       std::string_view commit_id,
+		                       std::error_code& ec);
+
+		blob_info verify(std::filesystem::path const& current_directory,
+		                 file_info const& file) const;
 	};
 }  // namespace cov::app::report
