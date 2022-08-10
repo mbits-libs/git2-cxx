0adc79a176938f8d57776b9c57638418f27771ad
Marcin Zdun <mzdun@midnightbits.com>
2022-08-10 07:35:30 +0200

    build(core): add `cov log` empty implementation

---
+++ apps/builtins/builtin_log.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Marcin Zdun
+// This code is licensed under MIT license (see LICENSE for details)
+
+#include <cov/app/args.hh>
+
+namespace cov::app::builtin::log {
+	int handle(std::string_view tool, args::arglist args) {
+		return 0;
+	}
+}  // namespace cov::app::builtin::log
