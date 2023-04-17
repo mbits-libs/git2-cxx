407481a6ed260fca7b709194715730ec40c6a8b0
Marcin Zdun <mzdun@midnightbits.com>
2023-04-17 07:49:21 +0200

    feat(rt): backend for tag and branch commands

---
+++ libs/cov-rt/include/cov/app/branches.hh
@@ -0,0 +1,57 @@
+// Copyright (c) 2022 Marcin Zdun
+// This code is licensed under MIT license (see LICENSE for details)
+
+#pragma once
+
+#include <cov/app/args.hh>
+#include <cov/app/cov_log_tr.hh>
+#include <cov/app/cov_module_tr.hh>
+#include <cov/app/errors_tr.hh>
+#include <cov/format.hh>
+#include <cov/format_args.hh>
+#include <filesystem>
+#include <span>
+
+namespace cov::app::platform {
+	bool glob_matches(std::filesystem::path const& match,
+	                  std::filesystem::path const name);
+}  // namespace cov::app::platform
+
+namespace cov::app {
+	enum class command { unspecified, list, create, remove, current };
+	enum class target : bool { tag = false, branch = true };
+
+	using branches_base_parser = base_parser<errlng, modlng, loglng>;
+
+	struct params {
+		target tgt;
+		std::vector<std::string> names{};
+		command cmd{command::unspecified};
+		bool force{false};
+		bool quiet{false};
+		color_feature color{};
+
+		void setup(::args::parser& parser, branches_base_parser& cli);
+		void validate_using(branches_base_parser& cli);
+		std::error_code run(cov::repository const&) const;
+
+	private:
+		template <command Arg>
+		auto set_command(branches_base_parser& cli) {
+			return [this, &cli]() { set_command(cli, Arg); };
+		}
+		void set_command(branches_base_parser& cli, command cmd);
+
+		auto add_name() {
+			return [this](std::string const& name) {
+				if (cmd == command::unspecified) cmd = command::create;
+				names.push_back(name);
+			};
+		}
+
+		void list(cov::repository const&) const;
+		std::error_code create(cov::repository const&) const;
+		std::error_code remove(cov::repository const&) const;
+		void show_current(cov::repository const&) const;
+	};
+}  // namespace cov::app
---
+++ libs/cov-rt/src/branches.cc
@@ -0,0 +1,254 @@
+// Copyright (c) 2022 Marcin Zdun
+// This code is licensed under MIT license (see LICENSE for details)
+
+#include <fmt/format.h>
+#include <cov/app/branches.hh>
+#include <cov/app/path.hh>
+#include <cov/format.hh>
+#include <cov/repository.hh>
+
+namespace fs = std::filesystem;
+using namespace std::literals;
+
+#ifdef WIN32
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
+namespace cov::app::platform {
+	bool glob_matches(fs::path const& match, fs::path const name) {
+		return !!PathMatchSpecW(name.c_str(), match.c_str());
+	}
+}  // namespace cov::app::platform
+#else
+#include <fnmatch.h>
+int fnmatch(const char* pattern, const char* string, int flags);
+namespace cov::app::platform {
+	bool glob_matches(fs::path const& match, fs::path const name) {
+		return !fnmatch(match.c_str(), name.c_str(), FNM_PATHNAME);
+	}
+}  // namespace cov::app::platform
+#endif
+
+namespace cov::app {
+	namespace {
+		static constexpr auto heads = "refs/heads"sv;
+		static constexpr auto tags = "refs/tags"sv;
+
+		inline branches_base_parser::string name_of(command cmd) {
+			using enum command;
+			switch (cmd) {
+				case create:
+					return str::args::lng::NAME_META;
+				case list:
+					return "--list"sv;
+				case remove:
+					return "--delete"sv;
+				case current:
+					return "--show-current"sv;
+				case unspecified:  // GCOV_EXCL_LINE
+					[[unlikely]];  // GCOV_EXCL_LINE
+					break;         // GCOV_EXCL_LINE
+			}
+			[[unlikely]];  // GCOV_EXCL_LINE
+			return {};     // GCOV_EXCL_LINE
+		}
+
+		std::string name_for(std::string_view name, target tgt) {
+			return fmt::format("{}/{}"sv, tgt == target::tag ? tags : heads,
+			                   name);
+		}
+
+		struct lister {
+			using color = placeholder::color;
+
+			std::string_view prefix{};
+			bool index{};
+			std::string_view current{};
+			std::string (*colorize)(color, void* app) = {};
+
+			void list_refs(cov::repository const& repo,
+			               std::span<std::string const> const& names);
+
+		private:
+			bool matching(std::vector<std::filesystem::path> const& matches,
+			              std::string_view name);
+			void print_selected(std::string_view name);
+		};
+
+		void lister::list_refs(cov::repository const& repo,
+		                       std::span<std::string const> const& names) {
+			std::vector<fs::path> matches{};
+			matches.reserve(names.size());
+			std::transform(names.begin(), names.end(),
+			               std::back_inserter(matches), make_u8path);
+
+			if (index && current.empty() && matching(matches, "HEAD"sv)) {
+				print_selected("(detached HEAD)"sv);
+			}
+
+			auto items = repo.refs()->iterator(prefix);
+			std::vector<std::string> matching_names{};
+			for (auto const ref : *items) {
+				auto const name = ref->shorthand();
+				if (!matching(matches, name)) continue;
+				matching_names.emplace_back(name.data(), name.size());
+			}
+
+			std::sort(matching_names.begin(), matching_names.end());
+			if (index) {
+				for (auto const& name : matching_names) {
+					if (current == name)
+						print_selected(name);
+					else
+						fmt::print("  {}\n"sv, name);
+				}
+			} else {
+				for (auto const& name : matching_names)
+					fmt::print("{}\n"sv, name);
+			}
+		}
+
+		bool lister::matching(std::vector<fs::path> const& matches,
+		                      std::string_view name) {
+			if (matches.empty()) return true;
+
+			auto const matched = make_u8path(name);
+			for (auto const& match : matches) {
+				if (platform::glob_matches(match, matched)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		void lister::print_selected(std::string_view name) {
+			if (!colorize) {
+				fmt::print("* {}\n"sv, name);
+				return;
+			}
+			fmt::print("* {}{}{}\n"sv, colorize(color::green, nullptr), name,
+			           colorize(color::reset, nullptr));
+		}
+	}  // namespace
+
+	void params::setup(::args::parser& parser, branches_base_parser& cli) {
+		parser.custom(add_name()).opt();
+		parser.custom(set_command<command::remove>(cli), "d", "delete").opt();
+		parser.custom(set_command<command::list>(cli), "l", "list").opt();
+		if (tgt == target::branch) {
+			parser.custom(set_command<command::current>(cli), "show-current")
+			    .opt();
+		}
+		parser.set<std::true_type>(force, "f", "force").opt();
+		if (tgt == target::branch) {
+			parser.set<std::true_type>(quiet, "q", "quiet").opt();
+			parser.arg(color, "color").opt();
+		}
+	}
+
+	void params::set_command(branches_base_parser& cli, command arg) {
+		if (cmd != command::unspecified) {
+			auto const& tr = cli.tr();
+			str_visitor visitor{tr};
+			cli.error(tr.format(str::args::lng::ARGUMENT_MSG,
+			                    visitor.visit(name_of(cmd)),
+			                    tr.format(str::args::lng::EXCLUSIVE,
+			                              visitor.visit(name_of(arg)))));
+		}
+		cmd = arg;
+	}
+
+	void params::validate_using(branches_base_parser& cli) {
+		if (cmd == command::unspecified) cmd = command::list;
+
+		auto const& tr = cli.tr();
+
+		if (force && cmd != command::create) {
+			str_visitor visitor{tr};
+			cli.error(tr.format(str::args::lng::ARGUMENT_MSG, "--force",
+			                    tr.format(str::args::lng::EXCLUSIVE,
+			                              visitor.visit(name_of(cmd)))));
+		}
+
+		std::optional<modcnt> error{};
+
+		if (cmd == command::create && names.size() != 1)
+			error = modcnt::ERROR_OPTS_NEEDS_EXACTLY;
+		else if (cmd == command::remove && names.empty())
+			error = modcnt::ERROR_OPTS_NEEDS_AT_LEAST;
+
+		if (error) {
+			str_visitor visitor{tr};
+			cli.error(fmt::format(fmt::runtime(tr(*error, 1)),
+			                      visitor.visit(name_of(cmd)), 1));
+		}
+	}
+
+	std::error_code params::run(cov::repository const& repo) const {
+		static const auto OK = std::error_code{};
+		switch (cmd) {
+			case command::list:
+				return list(repo), OK;
+			case command::create:
+				return create(repo);
+			case command::remove:
+				return remove(repo);
+			case command::current:
+				return show_current(repo), OK;
+			case command::unspecified:
+				break;
+		}
+		return OK;
+	}
+
+	void params::list(cov::repository const& repo) const {
+		if (tgt == target::tag) {
+			lister{.prefix{tags}}.list_refs(repo, names);
+			return;
+		}
+
+		auto const HEAD = repo.current_head();
+		auto clr = color;
+		if (clr == use_feature::automatic) {
+			clr = cov::platform::is_terminal(stdout) ? use_feature::yes
+			                                         : use_feature::no;
+		}
+		auto colorize =
+		    clr == use_feature::yes ? formatter::shell_colorize : nullptr;
+
+		lister{.prefix = heads,
+		       .index = true,
+		       .current = HEAD.branch,
+		       .colorize = colorize}
+		    .list_refs(repo, names);
+	}
+
+	std::error_code params::create(cov::repository const& repo) const {
+		auto const HEAD = repo.current_head();
+		if (!HEAD.tip) return git::make_error_code(git::errc::unbornbranch);
+		auto const full_name = name_for(names.front(), tgt);
+		if (!force) {
+			auto prev = repo.refs()->lookup(full_name);
+			if (prev) return git::make_error_code(git::errc::exists);
+		}
+
+		auto const fresh = repo.refs()->create(full_name, *HEAD.tip);
+		return fresh ? std::error_code{}
+		             : git::make_error_code(git::errc::invalidspec);
+	}
+
+	std::error_code params::remove(cov::repository const& repo) const {
+		auto const HEAD = repo.current_head();
+		auto curr = repo.refs()->lookup(name_for(names.front(), tgt));
+		if (!curr) return git::make_error_code(git::errc::notfound);
+
+		if (tgt == target::branch && HEAD.branch == curr->shorthand())
+			return cov::make_error_code(cov::errc::current_branch);
+
+		return repo.refs()->remove_ref(*curr);
+	}
+
+	void params::show_current(cov::repository const& repo) const {
+		auto const HEAD = repo.current_head();
+		if (!HEAD.branch.empty()) fmt::print("{}\n"sv, HEAD.branch);
+	}
+}  // namespace cov::app
