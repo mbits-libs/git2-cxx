9082d918838f5abdc152346ca086f92b3394c066
Marcin Zdun <mzdun@midnightbits.com>
2023-04-23 10:12:00 +0200

    feat(api): add error codes specific to cov-api

---
+++ libs/cov/src/error.cc
@@ -0,0 +1,38 @@
+// Copyright (c) 2023 Marcin Zdun
+// This code is licensed under MIT license (see LICENSE for details)
+
+#include <cov/error.hh>
+#include <string>
+
+namespace cov {
+	namespace {
+		class cov_category : public std::error_category {
+		public:
+			const char* name() const noexcept override {
+				return "cov::category";
+			}
+
+			std::string message(int value) const override {
+				using enum errc;
+				switch (static_cast<errc>(value)) {
+					case error:
+						return "Generic error";
+					case current_branch:
+						return "Current branch cannot be deleted";
+				}
+				return "cov::errc{" + std::to_string(value) + "}";
+			}
+
+			bool equivalent(int err, const std::error_condition& cond)
+			    const noexcept override {
+				return cond ==
+				       cov::make_error_condition(static_cast<errc>(err));
+			}
+		};
+	}  // namespace
+
+	std::error_category const& category() {
+		static cov_category cat;
+		return cat;
+	}
+}  // namespace cov
