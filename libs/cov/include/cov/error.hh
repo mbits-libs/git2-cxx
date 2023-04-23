9cab1e5fa0d4a9bc157c6c46f76b96fdd2841ea4
Marcin Zdun <mzdun@midnightbits.com>
2023-04-23 10:12:00 +0200

    feat(api): add error codes specific to cov-api

---
+++ libs/cov/include/cov/error.hh
@@ -0,0 +1,28 @@
+// Copyright (c) 2023 Marcin Zdun
+// This code is licensed under MIT license (see LICENSE for details)
+
+#pragma once
+
+#include <system_error>
+
+namespace cov {
+	enum class errc : int {
+		error = -1,
+		current_branch = -2,
+	};
+
+	std::error_category const& category();
+
+	inline std::error_code make_error_code(errc git_error) {
+		return std::error_code(static_cast<int>(git_error), cov::category());
+	}
+	inline std::error_condition make_error_condition(errc git_error) {
+		return std::error_condition(static_cast<int>(git_error),
+		                            cov::category());
+	}
+}  // namespace cov
+
+namespace std {
+	template <>
+	struct is_error_condition_enum<cov::errc> : public std::true_type {};
+}  // namespace std
---
+++ libs/cov/tests/error-test.cc
@@ -0,0 +1,37 @@
+// Copyright (c) 2022 Marcin Zdun
+// This code is licensed under MIT license (see LICENSE for details)
+
+#include <gtest/gtest.h>
+#include <cov/error.hh>
+
+namespace cov::testing {
+	using namespace ::std::literals;
+
+#define ON_ERRC(X) X(error) X(current_branch)
+
+	TEST(error, same_error) {
+#define X_ASSERT_EQ(ERR) ASSERT_EQ(errc::ERR, cov::make_error_code(errc::ERR));
+		ON_ERRC(X_ASSERT_EQ)
+#undef X_ASSERT_EQ
+	}
+
+	TEST(error, has_message) {
+#define X_HAS_MESSAGE(ERR) \
+	ASSERT_FALSE(cov::make_error_code(errc::ERR).message().empty());
+		ON_ERRC(X_HAS_MESSAGE)
+#undef X_HAS_MESSAGE
+	}
+
+	TEST(error, unknown_has_message) {
+		ASSERT_EQ("cov::errc{100}",
+		          cov::make_error_code(static_cast<errc>(100)).message());
+	}
+
+	TEST(error, category_has_name) {
+		ASSERT_STREQ(cov::category().name(),
+		             cov::make_error_code(errc::error).category().name());
+		// same pointer:
+		ASSERT_EQ(cov::category().name(),
+		          cov::make_error_code(errc::error).category().name());
+	}
+}  // namespace cov::testing
