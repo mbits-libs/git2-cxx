dc02d2e1be21b55d4f36e2be10ca330055a4bdce
Marcin Zdun <marcin.zdun@gmail.com>
2024-04-07 10:35:59 +0200

    Code signing on Windows
    Refs: #71

--- tools/flow/run.py
+++ tools/flow/run.py
@@ -15,7 +15,17 @@ import lib.runner as runner
 DEF_STEPS = {
     "config": ["Conan", "CMake"],
     "build": ["Build"],
-    "verify": ["Build", "Test", "Report", "Pack", "Store", "BinInst", "DevInst"],
+    "verify": [
+        "Build",
+        "Test",
+        "Report",
+        "Sign",
+        "Pack",
+        "SignPackages",
+        "Store",
+        "BinInst",
+        "DevInst",
+    ],
     "report": ["Build", "Test", "Report"],
 }
 cmd = os.path.splitext(os.path.basename(sys.argv[0]))[0]
@@ -309,4 +319,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
